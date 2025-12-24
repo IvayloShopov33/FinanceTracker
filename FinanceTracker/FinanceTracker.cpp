2c8de33d75d3925bf374584dc040e79c08c05a44
IvayloShopov <Shopov333@students.softuni.bg>
2025-12-25 00:04:26 +0200

    Add project files.

---
+++ FinanceTracker/FinanceTracker.cpp
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to course project # 03
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Ivaylo Shopov
+* @idnumber 2MI0600565
+* @compiler VC
+*
+* The implementation of the finance tracker project that helps users manage their personal finances.
+*
+*/
+
+#include <iostream>
+
+int main()
+{
+
+}
